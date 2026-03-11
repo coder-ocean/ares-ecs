cf18158f491ae84c05883dfd1246f198ba4de01b
Sagar <coder-ocean@LAPTOP-OJTMFSSQ.localdomain>
2026-03-12 02:05:13 +0530

    docker-build.tf corrected

--- docker-build.tf
+++ docker-build.tf
@@ -6,7 +6,7 @@ resource "null_resource" "build_push_backend" {
 
 aws ecr get-login-password --region ${var.region} | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.${var.region}.amazonaws.com
 
-docker build -t flask-backend ../backend
+docker build -t flask-backend /backend
 docker tag flask-backend:latest ${aws_ecr_repository.backend.repository_url}:latest
 docker push ${aws_ecr_repository.backend.repository_url}:latest
 
@@ -23,7 +23,7 @@ resource "null_resource" "build_push_frontend" {
 
     command = <<EOT
 
-docker build -t express-frontend ../frontend
+docker build -t express-frontend /frontend
 docker tag express-frontend:latest ${aws_ecr_repository.frontend.repository_url}:latest
 docker push ${aws_ecr_repository.frontend.repository_url}:latest
 
